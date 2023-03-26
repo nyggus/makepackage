e4b218122d17482057520d9c9700fac7e0e6aee1
Pato <boatengpato.pb@gmail.com>
2023-03-26 21:47:08 +0000

    used to store project metadata

---
+++ makepackage/write_setup_cfg.py
@@ -0,0 +1,42 @@
+import configparser
+from pathlib import Path
+
+
+def write_setup_cfg(path: Path, package_name: str, CLI: bool) -> None:
+    config = configparser.ConfigParser()
+
+    # Add sections to the ConfigParser object
+    config["metadata"] = {
+        "name": f"{package_name}",
+        "version": "0.1.0",
+        "author": "[MAKEPACKAGE]",
+        "author_email": "[MAKEPACKAGE]",
+        "description": "[MAKEPACKAGE]",
+        "long_description": "file: README.md",
+        "long_description_content_type": "text/markdown",
+        "url": "[MAKEPACKAGE]",
+        "license_files": "LICENSE",
+        "classifiers": [
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+    }
+
+    config["options"] = {
+        "packages": [f"{package_name}"],
+        "python_requires": ">=3.6",
+        "install_requires": ["easycheck"],
+    }
+
+    if CLI:
+        config["options.entry_points"] = {
+            f"{package_name}": f'"{package_name}.__main__:main"'
+        }
+
+    config["options.extras_require"] = {
+        "dev": ["wheel", "black", "pytest", "mypy"],
+    }
+
+    with open(path / "setup.cfg", "w") as f:
+        config.write(f)
