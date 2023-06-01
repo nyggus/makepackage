f2833d4a18a137e9276d574744be6de06aa8ea75
Pato <boatengpato.pb@gmail.com>
2023-06-01 16:23:01 +0000

    refactored code

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,80 +1,33 @@
-import pathlib
-import platform
-import pytest
-import shutil
-
 from typing import Dict, Iterator, List
 
-
-@pytest.fixture
-def testing_path() -> pathlib.Path:
-    return pathlib.Path(".") / "tests"
+import pytest
 
 
 @pytest.fixture
 def files_no_CLI() -> Iterator[Dict[str, List[str]]]:
-    if platform.system() == "Linux":
-        yield {
-            "root": [
-                "README.md",
-                ".gitignore",
-                "pytest.ini",
-                "setup.py",
-                "LICENSE",
-            ],
-            "src": ["__init__.py", "pkgNoCLI.py"],
-            "test": ["__init__.py", "conftest.py", "test_pkgNoCLI.py"],
-        }
-    else:
-        yield {
-            "root": [
-                "README.md",
-                ".gitignore",
-                "pytest.ini",
-                "setup.py",
-                "LICENSE",
-            ],
-            "src": ["__init__.py", "pkgNoCLIWin.py"],
-            "test": ["__init__.py", "conftest.py", "test_pkgNoCLIWin.py"],
-        }
+    return {
+        "root": [
+            "README.md",
+            ".gitignore",
+            "pytest.ini",
+            "setup.py",
+            "LICENSE",
+        ],
+        "src": ["__init__.py", "pkgNoCLI.py"],
+        "test": ["__init__.py", "conftest.py", "test_pkgNoCLI.py"],
+    }
 
 
 @pytest.fixture
 def files_with_CLI() -> Iterator[Dict[str, List[str]]]:
-    if platform.system() == "Linux":
-        yield {
-            "root": [
-                "README.md",
-                ".gitignore",
-                "pytest.ini",
-                "setup.py",
-                "LICENSE",
-            ],
-            "src": ["__init__.py", "__main__.py", "pkgWithCLI.py"],
-            "test": ["__init__.py", "conftest.py", "test_pkgWithCLI.py"],
-        }
-    else:
-        yield {
-            "root": [
-                "README.md",
-                ".gitignore",
-                "pytest.ini",
-                "setup.py",
-                "LICENSE",
-            ],
-            "src": ["__init__.py", "__main__.py", "pkgWithCLIWin.py"],
-            "test": ["__init__.py", "conftest.py", "test_pkgWithCLIWin.py"],
-        }
-
-    # Cleanup
-    for dir in (
-        "pkgNoCLI",
-        "venv-pkgNoCLI",
-        "pkgWithCLI",
-        "venv-pkgWithCLI",
-        "pkgNoCLIWin",
-        "venv-pkgNoCLIWin",
-        "pkgWithCLIWin",
-        "venv-pkgWithCLIWin",
-    ):
-        shutil.rmtree(pathlib.Path("tests") / dir, ignore_errors=True)
+    return {
+        "root": [
+            "README.md",
+            ".gitignore",
+            "pytest.ini",
+            "setup.py",
+            "LICENSE",
+        ],
+        "src": ["__init__.py", "__main__.py", "pkgWithCLI.py"],
+        "test": ["__init__.py", "conftest.py", "test_pkgWithCLI.py"],
+    }
