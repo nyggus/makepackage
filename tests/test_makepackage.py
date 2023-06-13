71dbb5bfc2c6e35e2200fc3f6b74feb729e211d7
Pato <boatengpato.pb@gmail.com>
2023-06-13 22:02:41 +0000

    added more functionality to tests

--- tests/test_makepackage.py
+++ tests/test_makepackage.py
@@ -7,45 +7,56 @@ import pytest
 
 
 def test_pkg_no_CLI(tmp_path: Path, files_no_CLI: Dict[str, List[str]]):
-    PY = "python" if platform.system() == "Windows" else "python3"
+    PY_CMD = "python" if platform.system() == "Windows" else "python3"
+    pkg_name = "pkgNoCLI"
+    venv = f"venv-{pkg_name}"
+    pkg_path = tmp_path / pkg_name
+    src_dir = tmp_path / pkg_name / pkg_name
+    tests_dir = tmp_path / pkg_name / "tests"
 
-    subprocess.run(f"{PY} -m venv venv-pkgNoCLI", shell=True, cwd=tmp_path)
-    subprocess.run("makepackage pkgNoCLI", shell=True, cwd=tmp_path)
+    subprocess.run(f"{PY_CMD} -m venv {venv}", shell=True, cwd=tmp_path)
+    subprocess.run(f"makepackage {pkg_name}", shell=True, cwd=tmp_path)
+    subprocess.run(f"{PY_CMD} -m pip install -e .", shell=True, cwd=pkg_path)
+    subprocess.run("pytest", shell=True, cwd=pkg_path)
+    subprocess.run(
+        f"{PY_CMD} -m doctest {src_dir / pkg_name}.py", shell=True, cwd=pkg_path
+    )
 
     files = files_no_CLI
 
-    assert (tmp_path / "venv-pkgNoCLI").exists()
-    assert (tmp_path / "pkgNoCLI").exists()
-    assert (tmp_path / "pkgNoCLI" / "pkgNoCLI").exists()
-    assert (tmp_path / "pkgNoCLI" / "tests").exists()
+    assert (tmp_path / venv).exists()
+    assert (pkg_path).exists()
+    assert (src_dir).exists()
+    assert (tests_dir).exists()
 
-    assert all((tmp_path / "pkgNoCLI" / file).exists() for file in files["root"])
-    assert all(
-        (tmp_path / "pkgNoCLI" / "pkgNoCLI" / file).exists() for file in files["src"]
-    )
-    assert all(
-        (tmp_path / "pkgNoCLI" / "tests" / file).exists() for file in files["test"]
-    )
+    assert all((pkg_path / file).exists() for file in files["root"])
+    assert all((src_dir / file).exists() for file in files["src"])
+    assert all((tests_dir / file).exists() for file in files["test"])
 
 
 def test_pkg_with_CLI(tmp_path: Path, files_with_CLI: Dict[str, List[str]]):
-    PY = "python" if platform.system() == "Windows" else "python3"
+    PY_CMD = "python" if platform.system() == "Windows" else "python3"
+    pkg_name = "pkgWithCLI"
+    venv = f"venv-{pkg_name}"
+    pkg_path = tmp_path / pkg_name
+    src_dir = tmp_path / pkg_name / pkg_name
+    tests_dir = tmp_path / pkg_name / "tests"
 
-    subprocess.run(f"{PY} -m venv venv-pkgWithCLI", shell=True, cwd=tmp_path)
-    subprocess.run("makepackage pkgWithCLI --cli", shell=True, cwd=tmp_path)
+    subprocess.run(f"{PY_CMD} -m venv {venv}", shell=True, cwd=tmp_path)
+    subprocess.run(f"makepackage {pkg_name} --cli", shell=True, cwd=tmp_path)
+    subprocess.run(f"{PY_CMD} -m pip install -e .", shell=True, cwd=pkg_path)
+    subprocess.run("pytest", shell=True, cwd=pkg_path)
+    subprocess.run(
+        f"{PY_CMD} -m doctest {src_dir / pkg_name}.py", shell=True, cwd=pkg_path
+    )
 
     files = files_with_CLI
 
-    assert (tmp_path / "venv-pkgWithCLI").exists()
-    assert (tmp_path / "pkgWithCLI").exists()
-    assert (tmp_path / "pkgWithCLI" / "pkgWithCLI").exists()
-    assert (tmp_path / "pkgWithCLI" / "tests").exists()
+    assert (tmp_path / venv).exists()
+    assert (pkg_path).exists()
+    assert (src_dir).exists()
+    assert (tests_dir).exists()
 
-    assert all((tmp_path / "pkgWithCLI" / file).exists() for file in files["root"])
-    assert all(
-        (tmp_path / "pkgWithCLI" / "pkgWithCLI" / file).exists()
-        for file in files["src"]
-    )
-    assert all(
-        (tmp_path / "pkgWithCLI" / "tests" / file).exists() for file in files["test"]
-    )
+    assert all((pkg_path / file).exists() for file in files["root"])
+    assert all((src_dir / file).exists() for file in files["src"])
+    assert all((tests_dir / file).exists() for file in files["test"])
